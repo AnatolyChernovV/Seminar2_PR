8603c58c07de2db6be1b874b2cebf1b9e5b4f104
Anatoly Chernov <chernv-tlja@rambler.ru>
2023-01-20 20:12:53 +0300

    task10, task 13, task 15 added

--- task15/Program.cs
+++ task15/Program.cs
@@ -8,31 +8,12 @@
 Console.Write("Введите число от 1 до 7: "); // Вывод в консоль
 int num = Convert.ToInt32(Console.ReadLine()); // Ввод числа
 
-if (num == 1)// если
+if (num >0 && num < 6)// если
     {
         Console.WriteLine("Нет");
     }
-else if (num == 2)// также если
-    {
-        Console.WriteLine("Нет");
-    }
-else if (num == 3)
-    {
-        Console.WriteLine("Нет");
-    }
-else if (num == 4)
-    {
-        Console.WriteLine("Нет");
-    }
-else if (num == 5)
-    {
-        Console.WriteLine("Нет");
-    }
-else if (num == 6)
-    {
-        Console.WriteLine("Да");
-    }
-else if (num == 7)
+    
+else if (num == 6 || num == 7 )
     {
         Console.WriteLine("Да");
     }
