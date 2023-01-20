05e410c83dd568d14293a3e139b04d300ca926a7
Anatoly Chernov <chernv-tlja@rambler.ru>
2023-01-20 19:44:17 +0300

    task10, task 13 added

---
+++ task10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число N: "); // Вывод в консоль
+int num = Convert.ToInt32(Console.ReadLine()); // Ввод числа
+int num1 = 0;
+if (99 < num && num < 1000 )
+    {
+        System.Console.WriteLine("Всё хорошо");
+        num = num % 100;
+        num1 = num % 10;
+        num = (num-num1)/10;
+        Console.WriteLine(num);
+    }
+else
+    {
+        System.Console.WriteLine("Вы ввелти не трехзначное число");
+    }
+
---
+++ task13/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Write("Введите число N: "); // Вывод в консоль
+int num = Convert.ToInt32(Console.ReadLine()); // Ввод числа
+int num1 = 0;
+if (99 < num && num < 1000)
+{
+    System.Console.WriteLine("Всё хорошо");
+    num = num % 10;
+    // num1 = num % 10;
+    // num = (num-num1)/10;
+    Console.WriteLine(num);
+            
+    
+}
+else
+    if (1000 < num && num < 10000)
+                     {
+                     System.Console.WriteLine("Всё хорошо");
+                     num = num % 100;
+                     num1 = num % 10;
+                     num = (num - num1) / 10;
+                     Console.WriteLine(num);
+                     }
+    else
+        if (10000 < num)
+                     {
+                     System.Console.WriteLine("Всё хорошо");
+                     num = num % 1000;
+                     num1 = num % 100;
+                     num = (num - num1) / 100;
+                     Console.WriteLine(num);
+                     }
+else
+    {
+        System.Console.WriteLine("третьей цифры нет");
+    }
+
